df8182ff1756fa570ce6c4af89f8744bad3e1724
Miroslava <miroslavaacuk475@gmail.com>
2024-06-23 13:56:37 +0300

    fixed header

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,15 @@
 let burger = document.querySelector('.burger');
 let header = document.querySelector('.header-content');
 let headerMain = document.querySelector('.header');
-let closeBtn = document.querySelector('.close-btn');
 
 burger.addEventListener('click', function() {
     burger.classList.toggle('active');
     header.classList.toggle('active');
     headerMain.classList.toggle('active');
-    closeBtn.classList.add('active');
 });
 
 closeBtn.addEventListener('click', function() {
     burger.classList.remove('active');
     header.classList.remove('active');
     headerMain.classList.remove('active');
-    closeBtn.classList.remove('active');
 });
