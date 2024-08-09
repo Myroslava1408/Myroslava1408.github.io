c51a7d8622eb9785146c8f3ac62f1b8420656646
Miroslava <miroslavaacuk475@gmail.com>
2024-08-09 15:22:58 +0300

    Fix animation

--- script.js
+++ script.js
@@ -1,51 +1,57 @@
-// An element in the visible part of the screen
+
 function isElementInViewport(el) {
     let rect = el.getBoundingClientRect();
     return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        rect.top < window.innerHeight &&
+        rect.bottom > 0
     );
 }
 
-// Text visibility checks
 function checkTextVisibility() {
     let paragraphs = document.getElementsByClassName('text-animation');
     for (let i = 0; i < paragraphs.length; i++) {
         if (isElementInViewport(paragraphs[i])) {
             paragraphs[i].classList.add('visible-text');
+        } else {
+            paragraphs[i].classList.remove('visible-text');
         }
     }
 }
 
-//Checking the visibility of the section
-function checkSectionVisibility() {
+function updateSectionStyles() {
     let section = document.querySelector('.section-second');
     if (section) {
-        let rect = section.getBoundingClientRect();
         let viewportHeight = window.innerHeight;
+        let scrollTop = window.scrollY;
+        let documentHeight = document.documentElement.scrollHeight;
 
-
-        if (rect.top <= 0 && rect.bottom >= viewportHeight) {
+        if (scrollTop > 0) {
             section.classList.add('expanded-section');
         } else {
+
+            section.classList.remove('expanded-section');
+        }
+
+
+        if (scrollTop + viewportHeight >= documentHeight) {
             section.classList.remove('expanded-section');
         }
     }
 }
 
+
 function onScroll() {
     requestAnimationFrame(() => {
+        updateSectionStyles();
         checkTextVisibility();
-        checkSectionVisibility();
     });
 }
 
 
-document.addEventListener('DOMContentLoaded', function() {
-    checkTextVisibility();
-    checkSectionVisibility();
-});
-
 window.addEventListener('scroll', onScroll);
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    updateSectionStyles();
+    checkTextVisibility();
+});
